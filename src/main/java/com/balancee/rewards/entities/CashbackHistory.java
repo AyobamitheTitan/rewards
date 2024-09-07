8d70afef4bd36215d1098bbb79f1255963bb307e
Oluwatimilehin <ayobamijijnr@gmail.com>
2024-09-07 14:17:56 +0100

    implement db models

---
+++ src/main/java/com/balancee/rewards/entities/CashbackHistory.java
@@ -0,0 +1,41 @@
+package com.balancee.rewards.entities;
+
+import java.util.Date;
+import java.util.UUID;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CashbackHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID transactionID;
+
+
+    @Column
+    public Date transactionDate;
+
+    @Column
+    public double amountEarned;
+
+    @Column
+    public String description;  
+    
+    @ManyToOne
+    @JoinColumn(name="customer_id")
+    private CustomerRewards customerRewards;
+}
---
+++ src/main/java/com/balancee/rewards/entities/CustomerRewards.java
@@ -0,0 +1,36 @@
+package com.balancee.rewards.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerRewards {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID customerId;
+
+    @Column
+    public double totalCashBack;
+
+    @Column
+    public double currentBalance;
+
+    @OneToMany(mappedBy= "customerRewards", cascade=CascadeType.ALL)
+    private List<CashbackHistory> cashbackHistory = new ArrayList<CashbackHistory>();
+}
